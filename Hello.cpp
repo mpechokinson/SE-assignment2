30e6278cf4bde4583d72b4593b9255de0e4daa5c
Miguel angel Pecho Kinson <mpechokinson@pvamu.edu>
2025-10-15 20:44:52 -0500

    Feature:I changed the first line in feature-2 to create conlfict

--- Hello.cpp
+++ Hello.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello World (This is for Main)";
+    cout << "Hello World (This is for feature-2)";
     cout<< "Hi, Miguel angel Pecho Kinson!";
     return 0;
 }
